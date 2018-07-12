3b7d7e27ccf61c9babe14a3176c5d83b181df56b
wujianming <wujianming@raycloud.com>
2018-07-12 10:09:58 +0800

    merge code

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+import {
+    createRouter
+} from './router'
+
+export function createApp() {
+    let router = createRouter();
+    let app = new Vue({
+        router,
+        render: h => h(App)
+    });
+    return {
+        app,
+        router
+    };
+};
